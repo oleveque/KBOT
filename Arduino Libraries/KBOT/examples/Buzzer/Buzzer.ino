ad100117d01ba19df04efb2ad2b78796167c3ebd
Olivier <leveque.olv@gmail.com>
2017-12-21 12:00:27 +0100

    Update BUZZER example

--- Arduino Libraries/KBOT/examples/Buzzer/Buzzer.ino
+++ Arduino Libraries/KBOT/examples/Buzzer/Buzzer.ino
@@ -5,49 +5,114 @@
  * Copyright : (c) 2017, Olivier Lévêque <oleveque@crans.org>           *
  ************************************************************************/
 #include <KBOT_CONFIG.h>
-#include <KBOT/utility/melody.h>
 
-int speaker = BUZZER;
-
-int MAX_COUNT = sizeof(melody)/2;
-long tempo = 10000;
-int rest_count = 50;
-int toneM = 0;
-int beat = 0;
-long duration  = 0;
-
+const int c = 261;
+const int d = 294;
+const int e = 329;
+const int f = 349;
+const int g = 391;
+const int gS = 415;
+const int a = 440;
+const int aS = 455;
+const int b = 466;
+const int cH = 523;
+const int cSH = 554;
+const int dH = 587;
+const int dSH = 622;
+const int eH = 659;
+const int fH = 698;
+const int fSH = 740;
+const int gH = 784;
+const int gSH = 830;
+const int aH = 880;
+ 
 void setup() {
-  pinMode(speaker, OUTPUT);
+  pinMode(BUZZER, OUTPUT);
 }
-
+ 
 void loop() {
-  playMelody();
+  //Play first section
+  firstSection();
+ 
+  //Play second section
+  secondSection();
+ 
+  //Variant 1
+  beep(f, 250);  
+  beep(gS, 500);  
+  beep(f, 350);  
+  beep(a, 125);
+  beep(cH, 500);
+  beep(a, 375);  
+  beep(cH, 125);
+  beep(eH, 650);
+  delay(500);
+ 
+  //Repeat second section
+  secondSection();
+ 
+  //Variant 2
+  beep(f, 250);  
+  beep(gS, 500);  
+  beep(f, 375);  
+  beep(cH, 125);
+  beep(a, 500);  
+  beep(f, 375);  
+  beep(cH, 125);
+  beep(a, 650);  
+  delay(650);
 }
-
-void playMelody() {
-  for (int i = 0; i < MAX_COUNT; i++) {
-    toneM = melody[i];
-    beat = beats[i];
-    duration = beat*tempo;
-    playTone();
-    delayMicroseconds(3000);
-  }
+ 
+void beep(int note, int duration) {
+  //Play tone on BUZZER
+  tone(BUZZER, note, duration);
+  delay(duration);
+ 
+  //Stop tone on BUZZER
+  noTone(BUZZER);
+  delay(50);
 }
-
-void playTone() {
-  long elapsed_time = 0;
-  if (toneM > 0) {
-    while (elapsed_time < duration) {
-      digitalWrite(speaker,HIGH);
-      delayMicroseconds(toneM / 2);
-      digitalWrite(speaker, LOW);
-      delayMicroseconds(toneM / 2);
-      elapsed_time += (toneM);
-    }
-   }
-   else {
-    for (int j = 0; j < rest_count; j++) {
-      delayMicroseconds(duration);
-    }
-  }
+ 
+void firstSection() {
+  beep(a, 500);
+  beep(a, 500);    
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);  
+  beep(a, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+  delay(500);
+  beep(eH, 500);
+  beep(eH, 500);
+  beep(eH, 500);  
+  beep(fH, 350);
+  beep(cH, 150);
+  beep(gS, 500);
+  beep(f, 350);
+  beep(cH, 150);
+  beep(a, 650);
+  delay(500);
+}
+ 
+void secondSection() {
+  beep(aH, 500);
+  beep(a, 300);
+  beep(a, 150);
+  beep(aH, 500);
+  beep(gSH, 325);
+  beep(gH, 175);
+  beep(fSH, 125);
+  beep(fH, 125);    
+  beep(fSH, 250);
+  delay(325);
+  beep(aS, 250);
+  beep(dSH, 500);
+  beep(dH, 325);  
+  beep(cSH, 175);  
+  beep(cH, 125);  
+  beep(b, 125);  
+  beep(cH, 250);  
+  delay(350);
 }
